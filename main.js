dbec6e479ec4e52b696fec2ad2b491cc37e506b2
soliman elamsry <solyana38@gmail.com>
2022-12-19 00:01:36 +0200

    make simple frontend

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+window.onload=function(){
+
+    let btn = document.getElementById("burger")
+    let ul = document.getElementById('ul')
+    console.log(btn)
+
+    btn.onclick = ()=>{
+        btn.classList.toggle("active")
+        ul.classList.toggle("active")
+    }
+    
+  }
